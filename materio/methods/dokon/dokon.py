82434d89dc1b64520f02c4105f419285d36cc855
Johadev05 <132278335+Johadev05@users.noreply.github.com>
2023-07-13 20:58:39 +0500

    Update dokon.py

--- materio/methods/dokon/dokon.py
+++ materio/methods/dokon/dokon.py
@@ -19,7 +19,7 @@ def add_dokon(request, params):
         return result
 
     all_info = next((field for field in [
-        "name", "location", "product", "employee_number", "savdo",
+        "name", "location", "product", "employee_number", "savdo_id",
         "product_number"
     ] if field not in params), '')
 
