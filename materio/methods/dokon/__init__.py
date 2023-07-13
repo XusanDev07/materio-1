17b1f4ceb4cfa7dedfa59058dc1b42c3621738cc
Johadev05 <132278335+Johadev05@users.noreply.github.com>
2023-07-13 18:54:28 +0500

    Update __init__.py

--- materio/methods/dokon/__init__.py
+++ materio/methods/dokon/__init__.py
@@ -1,3 +1,3 @@
-from materio.methods.dokon.savdo import add_savdo, savdo_ooynasi, buyurtma_oynasi
+from materio.methods.dokon.savdo import add_savdo, savdo_ooynasi
 from materio.methods.dokon.clent import get_clent, add_clent, delete_clent, update_clent
 from materio.methods.dokon.dokon import get_dokon, add_dokon, delete_dokon, update_dokon
