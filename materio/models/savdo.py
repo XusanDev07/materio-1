26ea3081c71714dacdb03de135f4fcd354036dbd
Johadev05 <132278335+Johadev05@users.noreply.github.com>
2023-07-13 18:58:04 +0500

    Update savdo.py

--- materio/models/savdo.py
+++ materio/models/savdo.py
@@ -7,7 +7,7 @@ from materio.models.clent import Client
 class savdo_oynasi(models.Model):
 
     product = models.ForeignKey(Maxsulot, on_delete=models.SET_NULL, null=True, blank=True)
-    clent_bolsa = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    clent_bolsa = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, blank=True)
 
     sotish_narxi = models.CharField(max_length=50)
     valyuta = models.CharField(max_length=128, choices=[
@@ -16,5 +16,13 @@ class savdo_oynasi(models.Model):
         ("UZS", "UZS")
     ])
 
+    def x_format(self):
+        return {
+            "id": self.id,
+            "product": self.product,
+            "clent": self.clent_bolsa,
+            "sotish_narxi": self.sotish_narxi,
+            "valyuta": self.valyuta
+    
     def __str__(self):
         return self.product
