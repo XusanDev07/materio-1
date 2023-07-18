4c2b8af9ebd50e51941f7614f5a5443e48149c73
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-07-18 19:06:47 +0500

    Update home_page.py

--- materio/methods/direktor/home_page.py
+++ materio/methods/direktor/home_page.py
@@ -12,13 +12,8 @@ def magazin_inspection(request):
         return custom_response(False, message=MESSAGE['PermissionDenied'])
     return custom_response(True)
 
-#
-# ombor qo'shish' update qilish chetan qabul qilinan mahsulotlar ro'yxati omborga buyurtma' ombor yoki do'konga '
-# shularni ertaga boshlanishiga yozishim kereak keyin filter qilishim kerak do'konni chatgptida filtr' \
-#                                                                             ' qilingan kodlari bor'
-# keyin do'kon ro'yhati va allanbalo narsalani qilishim kerak tzda direktr sahifasida do'konlar sahifasi deb yozilgani \'
-# hammasini
-# bulani ichida do'
-# bulani ichida do'
-#
-# ertaga Ustozdan kassa(tushm, chiqim)->> valyuta tizimini keyin savdo modelari va funksiyasi bo'yicha so'rashim kere
+
+def ombor_inspection(request):
+    if request.user.user_type != [2, 3]:
+        return custom_response(False, message=MESSAGE['PermissionDenied'])
+    return custom_response(True)
