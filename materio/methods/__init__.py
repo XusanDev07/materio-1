931b21257a1cc7b172ae71c32321886ec544aa6c
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-07-19 14:56:18 +0500

    Update __init__.py

--- materio/methods/__init__.py
+++ materio/methods/__init__.py
@@ -2,7 +2,7 @@ from materio.methods.auth import regis, login, StepOne, StepTwo, logout, user_up
 from materio.methods.ombor import add_savat, del_savat, get_savat, add_ombor, get_ombor, update_ombor, \
     delete_ombor, omborga_buyurtma, get_storage_order, delete_storge_order
 from materio.methods.direktor import add_xodim, get_xodim, update_xodim, add_product, get_product, update_product,\
-    delete_product
+    delete_product, kassa
 from materio.methods.dokon import add_savdo, savdo_ooynasi, get_clent, add_clent, delete_clent, update_clent,\
     get_dokon, add_dokon, delete_dokon, update_dokon
 
