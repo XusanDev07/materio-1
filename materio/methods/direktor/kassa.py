71bb8ba58cb8967e012b822c2cd479081ca59fca
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-07-19 17:48:19 +0500

    Update kassa.py

--- materio/methods/direktor/kassa.py
+++ materio/methods/direktor/kassa.py
@@ -5,10 +5,12 @@ def kassa(request, params):
   tushumlar = savdo_oynasi.objects.get(sotish_narxi=params['sotish_narxi'])
   if not tushumlar:
     return custom_response(stauts=False, message={"error": "Bunday savdo amalga oshirilmagan"})
+    
   chiqimlar = chetdan_buyurtma.objects.get(narxi=params['chiqimlar_narxi'])
   if not chiqimlar:
     return custom_response(status=False, message={"error": "bunday buyurtma kampaniya tomonidan amalga oshirilmagan"})
-  foyda = tushumlar - chiqimlar
+    
+  foyda = int(tushumlar) - int(chiqimar)
   Kassa.objects.create(tushumlar=tushumlar, chiqimlar=chiqimlar, foyda=foyda)
   
   return custom_response(status=True, message={"Malumot saqlandi"})
