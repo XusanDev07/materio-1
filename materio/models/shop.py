b8402e1c5f2bafb659e21f4bbb86b50b3ae48c3c
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-07-20 16:09:25 +0500

    Update shop.py

--- materio/models/shop.py
+++ materio/models/shop.py
@@ -16,7 +16,6 @@ class shop(models.Model):
             "name": self.name,
             "location": self.location,
             "employee_number": self.employee_number,
-            "product": self.product.product_name,
             "savdo": self.savdo.product,
             "product_number": self.product_number
         }
