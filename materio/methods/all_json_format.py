159572a807e898b7e4b8d8003200ba15c9353723
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-07-22 17:07:28 +0500

    Update all_json_format.py

--- materio/methods/all_json_format.py
+++ materio/methods/all_json_format.py
@@ -1,6 +1,6 @@
 from materio.models.base import Maxsulot
 from materio.models.base import Storage, Storage_order
-
+from materio.models import shop
 
 def maxsulot_format(data: Maxsulot):
     return {
@@ -14,3 +14,23 @@ def maxsulot_format(data: Maxsulot):
         "entry_price": data.entry_price,
         "entry_price_type": data.entry_price_type
     }
+
+
+def dokon_maxsulot(request, data:shop, pk):
+    a = Maxsulot.objects.filter(id=pk).first()
+    if a:
+         return {
+            "id": a.id,
+            "product_name": a.product_name,
+            "size": a.size,
+            "color": a.color,
+            "joyi": a.joyi,
+            "soni": a.soni,
+            "product_price": a.product_price,
+            "entry_price": a.entry_price,
+            "product_price_type": a.product_price_type,
+            "entry_price_type": a.entry_price_type
+        }
+    else:
+        return custom_response(status=False, message={"Error": "Id kirib kemadi shunga malumotlani chiqaromimiz"})
+    
