5b6670b85db690c476a58d71c3c95259ce2b1c51
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-07-22 17:14:16 +0500

    Update urls.py

--- materio/urls.py
+++ materio/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from materio.views import Main
-from materio.methods.all_json_format import dokon_maxsulot
+# from materio.methods.all_json_format import dokon_maxsulot
 
 urlpatterns = [
     path('main/', Main.as_view()),
-    path('dokon_maxsulot/<int:pk>/', dokon_maxsulot),
+    # path('dokon_maxsulot/<int:pk>/', dokon_maxsulot),
 ]
