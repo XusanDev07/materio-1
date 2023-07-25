b7e9f83ca35b0ca1c8a6ba78eb18c38a8c4825c9
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-07-25 18:07:20 +0500

    Update savdo.py

--- materio/methods/dokon/savdo.py
+++ materio/methods/dokon/savdo.py
@@ -5,6 +5,7 @@ from materio.models import Client
 from materio.methods.direktor.home_page import magazin_inspection
 from methodism import custom_response
 
+
 def savdo_ooynasi(request, params):
     result = magazin_inspection(request)
     if not result['status']:
@@ -19,9 +20,10 @@ def add_savdo(request, params):
     result = magazin_inspection(request)
     if not result['status']:
         return result
-    if not Client.objects.filter(phone=request.user.phone).first():
+    a = Client.objects.filter(phone=request.user.phone).first()
+    if a in None:
         return custom_response(status=False, message={"Siz clentlar ro'yxatida yo'qsiz"})
-    
+
     product = Maxsulot.objects.get(product_name=params['product_name'])
     clent = Client.objects.get(name=params['clent_name'])
     sotish_narx = params['sotish_narxi']
@@ -29,4 +31,3 @@ def add_savdo(request, params):
 
     savdo_oynasi.objects.get_or_create(product=product, clent_bolsa=clent, sotish_narxi=sotish_narx, valyuta=valyuta)
     return custom_response(status=True, message={"Qilgan savdoyingiz uchun raxmat"})
-
