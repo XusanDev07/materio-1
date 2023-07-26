3717146b7ccf64398fce9386b778d08d7423c414
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-07-26 17:16:38 +0500

    Update home_page.py

--- materio/methods/direktor/home_page.py
+++ materio/methods/direktor/home_page.py
@@ -8,7 +8,7 @@ def direc_inspection(request):
 
 
 def magazin_inspection(request):
-    if request.user.user_type != 3:
+    if request.user.user_type != 1:
         return custom_response(False, message=MESSAGE['PermissionDenied'])
     return custom_response(True)
 
