627c2f484ca5c50753727b24eed25986e5d00cba
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-07-27 14:55:53 +0500

    all

--- materio/methods/__init__.py
+++ materio/methods/__init__.py
@@ -2,7 +2,7 @@ from materio.methods.auth import regis, login, StepOne, StepTwo, logout, user_up
 from materio.methods.ombor import add_savat, del_savat, get_savat, add_ombor, get_ombor, update_ombor, \
     delete_ombor, omborga_buyurtma, get_storage_order, delete_storge_order
 from materio.methods.direktor import add_xodim, get_xodim, update_xodim, add_product, get_product, update_product,\
-    delete_product, kassa
+    delete_product, kassa, get_chetdan_buyurtma, add_chetdan_buyurtma
 from materio.methods.dokon import add_savdo, savdo_ooynasi, get_clent, add_clent, delete_clent, update_clent,\
     get_dokon, add_dokon, delete_dokon, update_dokon
 from materio.methods.direktor.kassa import tushumlar
@@ -10,6 +10,7 @@ from materio.methods.all_json_format import dokon_ombor, get_ombor_buyurtma
 from materio.methods.direktor.home_page import get_ombor_clent_dokon
 from materio.methods.ombor.buyurtmalar import add_storage_order
 from materio.methods.direktor.buyutma import add_chetdan_buyurtma
+
 
 ununsable_variable = dir()
 
--- materio/methods/direktor/__init__.py
+++ materio/methods/direktor/__init__.py
@@ -1,3 +1,4 @@
 from materio.methods.direktor.xodimlar import add_xodim, get_xodim, update_xodim
 from .product import get_product, add_product, update_product, delete_product
 from materio.methods.direktor.kassa import kassa
+from materio.methods.direktor.buyutma import get_chetdan_buyurtma, add_chetdan_buyurtma
