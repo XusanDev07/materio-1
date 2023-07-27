c851c11f5b8b9743f311477cab27f56b72e23cf3
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-07-27 14:48:41 +0500

    add

--- materio/methods/direktor/buyutma.py
+++ materio/methods/direktor/buyutma.py
@@ -1,3 +1,5 @@
+from methodism import custom_response
+
 from materio.methods.direktor.home_page import direc_inspection
 from materio.models import chetdan_buyurtma
 
@@ -10,3 +12,20 @@ def get_storage_order(request, params):
     return {
         "result": [x.chetdan_buyurtma_format() for x in chetdan_buyurtma.objects.all()]
     }
+
+
+def add_chetdan_buyurtma(request, params):
+    sh = params['shartnome_raqami']
+    d = params['davlat_nomi']
+    z = params['zavod_nomi']
+    date = params['date']
+    h = params['holati']
+    m = params['maxsulot'],
+    ms = params['maxsulot_soni']
+    n = params['narxi']
+
+    if not sh or d or z or date or h or m or ms or n:
+        return custom_response(status=False, message={'Error': "malumotlar yetarli emas to'ldir"})
+
+    chetdan_buyurtma.objects.create(sh, d, z, date, h, m, ms, n)
+    return custom_response(status=True, message={"Succes": "Chet elga Buyurtma qilindi !"})
